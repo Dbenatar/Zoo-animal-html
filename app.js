88d85b0a80734a5694f6f37f855f8cb55c46fe1b
Dominic <dom.benatar@gmail.com>
2023-09-14 20:30:56 +0100

    fixed js box, still getting taken to new page. Must fix

--- app.js
+++ app.js
@@ -3,22 +3,21 @@
 let message;
 function greet() {
 let username = prompt("Hi, what is your name?");
-//console.log("Hello " + username);
 document.write("Hello, " + username);
 }
 
 function askFood() {
-    let food = prompt("How many bananas? (0-10)");
+    let food = prompt("How many bananas do you want to feed the panda's? (0-10)");
         let message;
 
-    if (food >= 5) {
-        message = "Boooooo";
-    } else if (food <= 8) {
-        message = "More please!";
-    } else if (food < 10) {
+    if (food > 5) {
+     message = "Boooooo";
+    } else if (food < 5) {
+     message = "More please!";
+    } else if (food <= 10) {
         message = "Thanks, these will not go on my pizza";
     } else {
-        message = "You better not be putting these on a pizza!";
+       message = "You better not be putting these on a pizza!";
 }
 
 document.write(message);
