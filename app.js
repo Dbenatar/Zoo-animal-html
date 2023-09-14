e41bf6aded56ca8ff6e3f7cadaf77889317be1c4
Dominic <dom.benatar@gmail.com>
2023-09-14 14:39:23 +0100

    Getting Js to work. Greeting and button

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-let person = prompt ("On a scale of 1-10, how much do you like cute animals") 
+// let person = prompt ("On a scale of 1-10, how much do you like cute animals") 
 
+let message;
+function greet() {
+let username = prompt("Hi, what is your name?");
+//console.log("Hello " + username);
+document.write("Hello " + username);
+}
+
+function askFood() {
+    let food = prompt("How many bananas? (0-20)");
+        let message;
+    if (food <= 10) {
+        message = "Boooooo";
+    } else if (food <= 15) {
+        message = "More please!";
+    } else if (food < 15) {
+        message = "Thanks, these will not go on my pizza";
+    }
+}
+
+document.write(message);
+
+message = "Hello " + username + "." ;
