452a1b8dd9f10646e77d9441a68671e152b3ba84
Dominic <dom.benatar@gmail.com>
2023-09-15 11:44:41 +0100

    added for loop

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 // let person = prompt ("On a scale of 1-10, how much do you like cute animals") 
 
-let message;
-function greet() {
-let username = prompt("Hi, what is your name?");
-document.write("Hello, " + username);
-}
+//let message;
+    //function greet() {
+//let username = prompt("Hi, what is your name?");
+//document.write("Hello, " + username);
+//}
 
-function askFood() {
-    let food = prompt("How many bananas do you want to feed the panda's? (0-10)");
-        let message;
+//function askFood() {
+    //let food = prompt("How many bananas do you want to feed the panda's? (0-10)");
+        //let message;
 
-    if (food > 5) {
-     message = "Boooooo";
-    } else if (food < 5) {
-     message = "More please!";
-    } else if (food <= 10) {
-        message = "Thanks, these will not go on my pizza";
-    } else {
-       message = "You better not be putting these on a pizza!";
-}
+    //if (food > 5) {
+     //message = "Boooooo";
+    //} else if (food < 5) {
+     //message = "More please!";
+    //} else if (food <= 10) {
+        //message = "Thanks, these will not go on my pizza";
+    //} else {
+       //message = "You better not be putting these on a pizza!";
+//}
 
-document.write(message);
+//document.write(message);
+//}
+
+function getBananna() {
+    let feeding = prompt("How many banannas 1-10?" ,"10");
+
+    for (let i = 0; i < feeding; i++) {
+        document.write("<img src='pizzapanda.png'/>")
+    }
 }
