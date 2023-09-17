5c557df24c50695909daca5363f1cdd486b1eb64
Dominic <dom.benatar@gmail.com>
2023-09-17 13:32:37 +0100

    updated for and while loop. Button responding better but not working properly

--- app.js
+++ app.js
@@ -6,32 +6,33 @@
 //document.write("Hello, " + username);
 //}
 
-//function askFood() {
-    //let food = prompt("How many bananas do you want to feed the panda's? (0-10)");
-        //let message;
+function askFood() {
+    let food = prompt("How many bananas do you want to feed the panda's? (0-10)");
+        let message;
 
-    //if (food > 5) {
-     //message = "Boooooo";
-    //} else if (food < 5) {
-     //message = "More please!";
-    //} else if (food <= 10) {
-        //message = "Thanks, these will not go on my pizza";
-    //} else {
-       //message = "You better not be putting these on a pizza!";
-//}
+    if (food < 5) {
+     message = "Moooore!";
+    } else if (food <= 5) {
+     message = "More please!";
+    } else if (food >= 10) {
+        message = "Thanks, these will not go on my pizza";
+    } else {
+       message = "You better not be putting these on a pizza!";
+}
 
-//document.write(message);
-//}
+document.write(message);
+}
 
 // Loop working. I want to apply CSS to diplay the resulting images in a nicer way.
 function getBananna() {
-    let feeding = prompt("How many banannas 1-10?");
+    let feeding = prompt("How many to go on the pizza?");
 
     while (feeding < 0 || feeding >= 10) {
         feeding = prompt("Please enter 10 or lower!")
-     document.write("<img src='pizzapanda.png'/>")
     }
-    //for (let i = 0; i < feeding; i++) {
         //document.write("<img src='pizzapanda.png'/>")
-    }
     
+    for (let i = 0; i < feeding; i++) {
+        document.write("<img src='pizzapanda.png'/>")
+    }
+}
